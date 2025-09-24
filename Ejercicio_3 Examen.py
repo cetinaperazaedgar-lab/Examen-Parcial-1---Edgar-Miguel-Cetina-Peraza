86ce81feaa5b2a4ee4eea40c97a7eba35c935475
Edgar Miguel Cetina Peraza <cetinaperazaedgar@gmail.com>
2025-09-24 16:47:12 -0600

    Update Ejercicio_3 Examen.py

--- Ejercicio_3 Examen.py
+++ Ejercicio_3 Examen.py
@@ -1,26 +1,16 @@
-def D_B_E(n):
+def D_B(n):
     if n == 0:
         return ""
-    return D_B_E(n // 2) + str(n % 2)
+    return D_B(n // 2) + str(n % 2)
 
-def D_B (num, precision=15):
-    entero = int(num)
-    decimal = num - entero
-    
-    bin_entero = D_B_E(entero)
-    if bin_entero == "":
-        bin_entero = "0"
-    
-    bin_decimal = ""
-    while decimal > 0 and len(bin_decimal) < precision:
-        decimal *= 2
-        bit = int(decimal)
-        bin_decimal += str(bit)
-        decimal -= bit
-    
-    return bin_entero + ("." + bin_decimal if bin_decimal else "")
-print("="*30)
-print("Conversor de decimal a binario")
-num = float(input("\nIntroduce un número: "))
-print("Equivalente en binario:", D_B(num))
-print("="*30)
+print("=" * 30)
+print("Conversor de decimal a binario (solo enteros positivos)")
+num = int(input("\nIntroduce un número entero positivo: "))
+
+if num < 0:
+    print("\nPor favor, introduce un número entero positivo.")
+elif num == 0:
+    print("\nEquivalente en binario: 0")
+else:
+    print("\nEquivalente en binario:", D_B(num))
+print("=" * 30)
